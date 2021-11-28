c7103eb40c2ea5467328825a697a58908cdf219a
Susan Wairimu <mwihaki2susan@gmail.com>
2021-11-28 10:32:27 +0300

    database

--- database/data.sql
+++ database/data.sql
@@ -13,4 +13,21 @@ _subject  varchar(20) NOT NULL,
 _message varchar(200) NOT NULL
 )
 
-INSERT INTO 
+INSERT INTO announce VALUES ('administrator','8882', 'complain', 'sms')
+
+
+CREATE TABLE capacity(
+id int NOT NULL PRIMARY KEY,
+number int NOT NULL
+)
+
+INSERT INTO capacity VALUES(1, 500),(2, 750),(3, 1000),(4, 1500)
+
+CREATE TABLE court (
+National_Id int NOT NULL PRIMARY KEY,
+File_number varchar(14) NOT NULL,
+Dateoftrail date NOT NULL,
+Sentence varchar(50) NOT NULL,
+Location_ varchar(20) NOT NULL
+)
+
