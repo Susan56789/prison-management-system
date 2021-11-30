5a5e277dce589b1d0a1d74a2c3e7844df04d1994
Susan Wairimu <mwihaki2susan@gmail.com>
2021-11-30 15:28:06 +0300

    prison management system

--- server.js
+++ server.js
@@ -6,8 +6,21 @@ const Admins = require('./database/Admins');
 
 
 
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(pino);
+app.use(express.json());
+app.use(express.urlencoded());
+
+
+app.post('/api/greeting', (req, res) => {
+  const name = req.query.name || 'World';
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify({ greeting: `Hello ${name}!` }));
+});
+
 //create admin
-let Wairimu = new Admins(4, 'Wairimu', 'Wairimu234')
+let Wairimu = new Admins(6, 'Wairimu', 'Wairimu234')
 
 
 dbOperation.getAdmin().then(res =>{
@@ -17,20 +30,9 @@ dbOperation.getAdmin().then(res =>{
 dbOperation.createAdmin(Wairimu);
 
 //console.log(Wairimu);
-{/**
-const app = express();
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(pino);
 
-app.get('/api/greeting', (req, res) => {
-  const name = req.query.name || 'World';
-  res.setHeader('Content-Type', 'application/json');
-  res.send(JSON.stringify({ greeting: `Hello ${name}!` }));
-});
+
 
 app.listen(3001, () =>
   console.log('Express server is running on localhost:3001')
 );
-
-
-*/}
