1d5a90820fa89eee26d89aba0afb44fa2b6c2838
Susan Wairimu <mwihaki2susan@gmail.com>
2022-01-14 13:16:19 +0300

    navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,38 +1,47 @@
-import React from 'react';
-import './styles.css';
-import './PerfomanceMeasures';
-import './Recruitment';
-import './employeeDetails';
-import './LeaveForm';
-import './Announce';
-
+import React from "react";
+import "./styles.css";
+import "./PerfomanceMeasures";
+import "./Recruitment";
+import "./employeeDetails";
+import "./LeaveForm";
+import "./Announce";
 
 const Navbar = () => {
-    return (
+  return (
+    <div>
+      <header>
         <div>
-         <header >
-      <div >
-        
-          <h3>
-            Prison Management System
-          </h3>
-        
-        <nav className='NavBar'>
+          <h3>Prison Management System</h3>
+
+          <nav className="navbar">
             <ul>
-                <li><a href="./PerfomanceMeasures">Performance Measure </a></li>
-                <li><a href="./Recruitment">Recruitment </a></li>
-                <li><a href="./employeeDetails">employee details </a></li>
-                <li><a href="./LeaveForm">leaveform </a></li>
-                <li><a href="./Announce">Feedback</a></li>
-                <li><a href="./Training">Training </a></li>
-                <li><a href="./Salary">Salary</a></li>
+              <li>
+                <a href="./PerfomanceMeasures">Performance Measure </a>
+              </li>
+              <li>
+                <a href="./Recruitment">Recruitment </a>
+              </li>
+              <li>
+                <a href="./employeeDetails">employee details </a>
+              </li>
+              <li>
+                <a href="./LeaveForm">leaveform </a>
+              </li>
+              <li>
+                <a href="./Announce">Feedback</a>
+              </li>
+              <li>
+                <a href="./Training">Training </a>
+              </li>
+              <li>
+                <a href="./Salary">Salary</a>
+              </li>
             </ul>
-        
-        </nav>
- </div>
-    </header>   
+          </nav>
         </div>
-    )
-}
+      </header>
+    </div>
+  );
+};
 
 export default Navbar;
